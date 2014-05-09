30d304e6336e92e752bc719a2bb681bb2f8f1d5b
ajohnson21 <aje221@Austens-MacBook-Air.local>
2014-05-09 09:53:38 -0400

    adding custom classes

---
+++ ALAiPadDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  ALAiPadDetailVC.h
+//  AudioList
+//
+//  Created by Austen Johnson on 5/9/14.
+//  Copyright (c) 2014 Austen Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALAiPadDetailVC : UIViewController
+
+@end
---
+++ ALAiPadListTableVC.h
@@ -0,0 +1,13 @@
+//
+//  ALAiPadListTableVC.h
+//  AudioList
+//
+//  Created by Austen Johnson on 5/9/14.
+//  Copyright (c) 2014 Austen Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALAiPadListTableVC : UITableViewController
+
+@end
